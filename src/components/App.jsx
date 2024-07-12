3209ac4aa6a3477c1e17d4a4ff3d1fc70b207f15
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-07-12 12:55:02 +0800

    delete header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,21 @@ import { getTrending } from "API/fetchAPI";
 import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import {SharedLayout} from './SharedLayout/SharedLayout';
+import {HomePage} from '../Pages/HomePage/Home';
+import {MovieDetails} from './MovieDetails/MovieDetails';
 
 export const App = () => {
 
   return (
+
 <Routes>
   <Route path="/" element={<SharedLayout />} />
+  <Route path="home" element={<HomePage/>}>
+  <Route path='movies/:movieId' element={<MovieDetails/>}/>
+  </Route>
+
+
 </Routes>
+
   );
 };
--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,12 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import {Header} from '../Header';
+import { Outlet, NavLink } from 'react-router-dom';
 
 export const SharedLayout = () => {
   return (
     <div>
-        <Header />
+        <header>
+            <nav>
+                <NavLink to="/" end>Home</NavLink>
+                <NavLink to='/movies'>Movies</NavLink>
+            </nav>
+        </header>
       {/* Outlet should be used in parent route elements to render their child route elements*/}
       <Outlet />
+      
       </div>
   )
 }
