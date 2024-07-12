b7dacf8dff768e43e334ac9fcfe2e6a4a179d510
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-07-12 13:17:39 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,7 @@
-import { getTrending } from "API/fetchAPI";
-import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import {SharedLayout} from './SharedLayout/SharedLayout';
-
 export const App = () => {
   return (
 <Routes>
   <Route path="/" element={<SharedLayout />} />
-  <Route index element={<HomePage/>}>
-  <Route path="`movies/:${movieID}`" element={<MovieDetails/>}/>
-  </Route>
-
-
 </Routes>
   );
 };
