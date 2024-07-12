e1f08a84b008871f1e76de8aa6282f7d9a921e30
JuliusB-ui <141216908+JuliusB-ui@users.noreply.github.com>
2024-07-12 14:00:26 +0800

    Revert "attempt in progress"
    This reverts commit 1394f6f39ab310043d24442d7f3986505c025cea.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
 export const App = () => {
   return (
-<Routes>
-  <Route path="/" element={<SharedLayout />} />
-</Routes>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
   );
 };
